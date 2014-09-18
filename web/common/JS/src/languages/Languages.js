79dd29a905539972b63f256563d439ca77eff5d8
Harshul Pandav <harshulpandav@gmail.com>
2014-09-18 18:56:05 -0400

    current language bug fixed

--- web/common/JS/src/languages/Languages.js
+++ web/common/JS/src/languages/Languages.js
@@ -8,7 +8,7 @@ goog.provide('owgis.languages');
 owgis.languages.buildselection = function buildDropDownLanguages(){
 	//Obtains the available languages
 	var languages = mapConfig.availableLanguages.split(";");
-	var currLang = mapConfig.defaultLanguage;
+	var currLang = _curr_language;
 	//Iterates ver all the available languages
 	for(var i = 0; i < languages.length; i++){
 		//Creates a new option
