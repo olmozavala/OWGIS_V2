9635a427a8a9fe4df14be4ab6837e506570dd10a
Olmo Zavala <olmozavala@gmail.com>
2014-09-25 18:55:09 -0400

    Put comments on helpTexts.js and fixed problem with the tooltip of the Calendars

--- web/common/JS/src/helpTexts/helpTexts.js
+++ web/common/JS/src/helpTexts/helpTexts.js
@@ -2,9 +2,9 @@ goog.provide('owgis.tooltips');
 
 owgis.tooltips.initHelpTexts = function initHelpTexts() {
 	
+	// When any of the windows move, it repositions the tooltip by reloading it.
+	// "toolTipWithImage are used when the text of the tooltip requires images
 	$('.toolTip, .toolTipWithImage').on("mousemove",function(){
-//		if(!$(this).attr('title')){return true;}
-//		alert($(this).html);
 			var disabled = $('.toolTip').uitooltip( "option", "disabled" );
 			if(!disabled){
 				$(this).uitooltip('close'); 
@@ -12,6 +12,7 @@ owgis.tooltips.initHelpTexts = function initHelpTexts() {
 			}
 	});
 	
+	//Initializes all the tooltips. By default they appear at the left bottom
 	$('.toolTip').uitooltip({
 		show: null, 
 		hide: null, 
@@ -19,13 +20,18 @@ owgis.tooltips.initHelpTexts = function initHelpTexts() {
 		position: { my: "left top+15", at: "left bottom" , collision: "flipfit"},
 			});
 	
+	// Updates the tooltips for the dragabble windows, the tooltip appears at
+	// the center in the right.
 	$('.toolTip.transDraggableWindow, .toolTip.draggableWindow' ).uitooltip({
 		show: null, 
 		hide: null, 
 		tooltipClass: 'commonHover',
 		position: { my: "left+15 center", at: "right center", collision: "flipfit" },
 			});
-//	
+
+
+	//For the tooltips that require images inside, we assign the contents
+	// of the the object with the id of title='id' and use that as a conent
 	$('.toolTipWithImage.transDraggableWindow, .toolTipWithImage.draggableWindow').uitooltip({
 		show: null, 
 		hide: null, 
@@ -36,15 +42,8 @@ owgis.tooltips.initHelpTexts = function initHelpTexts() {
 		 return $(tooltip_div).html();
 			}
 		});
+
+	// Remove all attributes 'title' from the childrens of the calendar containers
+	// If we don't do it, there are problems with the tooltips inside the datepickers
+	$("#CalendarsAndStopContainer [title]").removeAttr("title")
 }
-
-
-
-
-//function updateToolTipPos(){
-//	alert('tooltip moved');
-////	$('.toolTip').uitooltip('disable');
-////	$('.toolTipWithImage').uitooltip('disable');
-////	$('.toolTip').uitooltip('enable');
-////	$('.toolTipWithImage').uitooltip('enable');
-//}
