57ad1cdb2c2ef23ba3564bdbf003f05239ba66ce
Olmo Zavala Romero <olmozavala@gmail.com>
2017-09-12 17:00:30 -0500

    Updated Layer.java so that does not breaks with ncWMS2 with a layer that has min and max color values as integers

--- src/java/com/mapviewer/model/Layer.java
+++ src/java/com/mapviewer/model/Layer.java
@@ -283,8 +283,10 @@ public class Layer {
 			} else {
 				this.layerDetails = new JSONObject(layerDetailsStr);
 				if ((this.minColor == -1) && (this.maxColor == -1)) {
-					this.minColor = Float.parseFloat((String) (((JSONArray) layerDetails.get("scaleRange")).get(0)));
-					this.maxColor = Float.parseFloat((String) (((JSONArray) layerDetails.get("scaleRange")).get(1)));
+					Object minVal =  ((JSONArray) layerDetails.get("scaleRange")).get(0);
+					Object maxVal =  ((JSONArray) layerDetails.get("scaleRange")).get(0);
+					this.minColor = Float.parseFloat( minVal.toString());
+					this.maxColor = Float.parseFloat( maxVal.toString());
 				}
 			}
 
