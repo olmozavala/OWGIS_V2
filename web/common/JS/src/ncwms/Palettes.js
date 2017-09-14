f16255418595c3d6a21db04dc609389a0c02fd34
ixchel <ixchelzg@gmail.com>
2017-09-14 18:45:55 -0500

    paginacion de paletas de colores

--- web/common/JS/src/ncwms/Palettes.js
+++ web/common/JS/src/ncwms/Palettes.js
@@ -69,10 +69,78 @@ function setColorRangeFromMinMax(){
 	owgis.ajax.crossorigin(url,updateMinMaxFromJson);
 }
 
+/*
+ * Pagination for Palettes 
+ */
+var current_page = 1;
+var records_per_page = 10;
+//The 'default' style is defined in the MapViewerServlet
+origpalette = mappalette;
+
+function prevPage()
+{
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+    }
+}
+
+function nextPage()
+{
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page);
+    }
+}
+    
+function changePage(page)
+{
+    var btn_next = document.getElementById("btn_next");
+    var btn_prev = document.getElementById("btn_prev");
+    
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    //Inserts the optional palettes in a table
+    $("#palettesTable").empty();
+    var tableRow = $('#palettesTable')[0].insertRow(0);
+
+    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
+            
+        palstr = layerDetails.palettes[i];
+        if(typeof palstr !== "undefined"){
+            var td = document.createElement('td');
+            td.setAttribute('onclick',"UpdatePalette('"+palstr+"');");
+            td.innerHTML = "<img class='optPaletteImg' src='"+paletteUrl.replace(origpalette,palstr)+"' /></td>";
+            tableRow.appendChild(td);
+        }
+    }
+
+    if (page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page == numPages()) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
+}
+
+function numPages()
+{
+    return Math.ceil(layerDetails.palettes.length / records_per_page);
+}
+
 /**
  * Fills the dropdown menu that contains the available palettes
  */
 owgis.ncwms.palettes.loadPalettes = function(){
+    
+    changePage(1);
     
     //Copied from loadDefault
     urlPaletteImg = $('#imgPalette').attr("src");
@@ -92,6 +160,7 @@ owgis.ncwms.palettes.loadPalettes = function(){
     }
 
     //Inserts the optional palettes in a table
+    /*
     var tableRow = $('#palettesTable')[0].insertRow(0);
 
     for (var key in layerDetails.palettes) {
@@ -102,9 +171,10 @@ owgis.ncwms.palettes.loadPalettes = function(){
         td.innerHTML = "<img class='optPaletteImg' src='"+paletteUrl.replace(origpalette,palstr)+"' /></td>";
         tableRow.appendChild(td);
     }
-
+    */
     $('#minPal').val( parseFloat(minPalVal).toPrecision(4)); 
     $('#maxPal').val( parseFloat(maxPalVal).toPrecision(4));
+    
 }
 
 /**
