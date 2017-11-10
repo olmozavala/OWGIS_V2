25b850fce6ee381091e8889a64cbc32a85b83e1a
Olmo Zavala Romero <olmozavala@gmail.com>
2017-11-09 19:41:43 -0600

    Make Animations.js easier to maintain

--- web/common/JS/src/ncwms/animations/Animations.js
+++ web/common/JS/src/ncwms/animations/Animations.js
@@ -465,76 +465,32 @@ function canvasAnimationFunction(extent, resolution, pixelRatio, size, projectio
 	
 //		TIME: allFrames[0],
 	// Creating dhe default parameters for the images
-	if( layerDetails.aboveMaxColor[1] != null && layerDetails.belowMinColor[1] != null ){
-            animParams = { 
-                    TIME:allFrames[0],
-                    LAYERS: layerName,
-                    BBOX: bbox.toString(),
-                    REQUEST: "GetMap",
-                    VERSION: owgis.ogc.wmsversion,
-                    STYLES: lay_style+"/"+mappalette,
-                    FORMAT: "image/png",
-                    TRANSPARENT: "TRUE",
-                    WIDTH: imgWidth,
-                    HEIGHT: imgHeight,
-                    NUMCOLORBANDS: 250,
-                    COLORSCALERANGE:  minPalVal + ',' + maxPalVal,
-                    CRS: _map_projection,
-                    BELOWMINCOLOR: layerDetails.belowMinColor[1],
-                    ABOVEMAXCOLOR: layerDetails.aboveMaxColor[1]
-            };
-        } else if(layerDetails.belowMinColor[1] != null){
-            animParams = { 
-                    TIME:allFrames[0],
-                    LAYERS: layerName,
-                    BBOX: bbox.toString(),
-                    REQUEST: "GetMap",
-                    VERSION: owgis.ogc.wmsversion,
-                    STYLES: lay_style+"/"+mappalette,
-                    FORMAT: "image/png",
-                    TRANSPARENT: "TRUE",
-                    WIDTH: imgWidth,
-                    HEIGHT: imgHeight,
-                    NUMCOLORBANDS: 250,
-                    COLORSCALERANGE:  minPalVal + ',' + maxPalVal,
-                    CRS: _map_projection,
-                    BELOWMINCOLOR: layerDetails.belowMinColor[1]
-            };
-        } else if(layerDetails.aboveMaxColor[1] != null){
-            animParams = { 
-                    TIME:allFrames[0],
-                    LAYERS: layerName,
-                    BBOX: bbox.toString(),
-                    REQUEST: "GetMap",
-                    VERSION: owgis.ogc.wmsversion,
-                    STYLES: lay_style+"/"+mappalette,
-                    FORMAT: "image/png",
-                    TRANSPARENT: "TRUE",
-                    WIDTH: imgWidth,
-                    HEIGHT: imgHeight,
-                    NUMCOLORBANDS: 250,
-                    COLORSCALERANGE:  minPalVal + ',' + maxPalVal,
-                    CRS: _map_projection,
-                    ABOVEMAXCOLOR: layerDetails.aboveMaxColor[1]
-            };
-        } else {
-            animParams = { 
-		TIME:allFrames[0],
-		LAYERS: layerName,
-		BBOX: bbox.toString(),
-		REQUEST: "GetMap",
-		VERSION: owgis.ogc.wmsversion,
-		STYLES: lay_style+"/"+mappalette,
-		FORMAT: "image/png",
-		TRANSPARENT: "TRUE",
-		WIDTH: imgWidth,
-		HEIGHT: imgHeight,
-		NUMCOLORBANDS: 250,
-		COLORSCALERANGE:  minPalVal + ',' + maxPalVal,
-		CRS: _map_projection
-            };
-        }
+	// Setting the default parameters for the layers being requested in the animation
+	animParams = { 
+				TIME:allFrames[0],
+				LAYERS: layerName,
+				BBOX: bbox.toString(),
+				REQUEST: "GetMap",
+				VERSION: owgis.ogc.wmsversion,
+				STYLES: lay_style+"/"+mappalette,
+				FORMAT: "image/png",
+				TRANSPARENT: "TRUE",
+				WIDTH: imgWidth,
+				HEIGHT: imgHeight,
+				NUMCOLORBANDS: 250,
+				COLORSCALERANGE:  minPalVal + ',' + maxPalVal,
+				CRS: _map_projection,
+		};
 	
+	if( layerDetails.aboveMaxColor[1] !== null && layerDetails.belowMinColor[1] !== null ){
+            animParams.BELOWMINCOLOR = layerDetails.belowMinColor[1];
+            animParams.ABOVEMAXCOLOR = layerDetails.aboveMaxColor[1];
+        } else if(layerDetails.belowMinColor[1] !== null){
+            animParams.BELOWMINCOLOR = layerDetails.belowMinColor[1];
+        } else if(layerDetails.aboveMaxColor[1] !== null){
+            animParams.ABOVEMAXCOLOR = layerDetails.aboveMaxColor[1];
+		}	
+
 	if (layerDetails.zaxis !== undefined) {
 		animParams.elevation =  layerDetails.zaxis.values[owgis.ncwms.zaxis.globcounter];
 	}
