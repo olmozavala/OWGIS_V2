78703605e9f73df67500cef74b1c03264b287c8c
ixchel <ixchelzg@gmail.com>
2017-11-30 14:53:58 -0600

    updated cesium and 3d view now works.

--- web/common/JS/src/ol3/PopUp.js
+++ web/common/JS/src/ol3/PopUp.js
@@ -7,4 +7,12 @@ goog.provide('owgis.ol3.popup');
 owgis.ol3.popup.closePopUp = function (){
 	console.log("Closing popup punctual data");
     $('#popup').fadeOut();
+    
+    if(mobile){
+                document.getElementById("containerChartsTS").innerHTML = "";
+                document.getElementById("containerChartsVP").innerHTML = "";
+                document.getElementById("containerChartsTS").style.display = 'none';
+                document.getElementById("containerChartsVP").style.display = 'none';
+                document.getElementById("popup").style.width = "200px";
+        }
 }
