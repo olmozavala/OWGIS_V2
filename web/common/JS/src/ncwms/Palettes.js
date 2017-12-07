3921f77fe8c695f69a9513f8bdf21c926e65d3c7
ixchel <ixchelzg@gmail.com>
2017-12-07 14:16:58 -0600

    step of ranges changes between .1 and 1 depending on max and min color defined for layer

--- web/common/JS/src/ncwms/Palettes.js
+++ web/common/JS/src/ncwms/Palettes.js
@@ -367,6 +367,8 @@ function showPalettes()
  * @param amount - amount to change or update to 
  */
 function increaseMaxColorRange(amount){
+    if( (layerDetails.scaleRange[0] < 1 && layerDetails.scaleRange[0] > 0) || (layerDetails.scaleRange[1] < 1 && layerDetails.scaleRange[1] > 0) ){ amount = amount*0.1; } else { amount = amount*1; }
+
     $('#maxPal').val( eval(parseFloat($('#maxPal').val()).toPrecision(4)) + parseFloat(amount));
     UpdatePalette(mappalette);
 }
@@ -376,6 +378,8 @@ function increaseMaxColorRange(amount){
  * @param amount - amount to change or update to 
  */
 function decreaseMinColorRange(amount){
+    if( (layerDetails.scaleRange[0] < 1 && layerDetails.scaleRange[0] > 0) || (layerDetails.scaleRange[1] < 1 && layerDetails.scaleRange[1] > 0) ){ amount = amount*0.1; } else { amount = amount*1; }
+
     $('#minPal').val( eval(parseFloat($('#minPal').val()).toPrecision(4)) - parseFloat(amount));
     UpdatePalette(mappalette);
 }
