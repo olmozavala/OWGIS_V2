aa5763e3fba7bc8a585334c6a16091936fd85287
ixchel <ixchelzg@gmail.com>
2018-01-15 16:12:40 -0600

    now you can see optional layers on 3d

--- web/common/JS/src/ol3/MainOl3.js
+++ web/common/JS/src/ol3/MainOl3.js
@@ -95,6 +95,9 @@ function initOl3(){
 			changeProj = true;
 		}else{
 			defCenter= [lon,lat];
+                        if(typeof mapConfig.restrictedExtent !== 'undefined'){
+                            resExtent = ol.proj.transformExtent(mapConfig.restrictedExtent.split(",").map(Number), 'EPSG:4326', _map_projection);
+                        }
 		}
 	}else{
 		if( (_map_bk_layer === "osm") || 
@@ -110,6 +113,10 @@ function initOl3(){
 		defCenter = ol.proj.transform([lon, lat], 'EPSG:4326', _map_projection);
 		resExtent = ol.proj.transform(mapConfig.restrictedExtent.split(",").map(Number), 'EPSG:4326', _map_projection);
 	}
+        
+        if(typeof mapConfig.restrictedExtent !== 'undefined' && typeof resExtent !== 'undefined'){
+            resExtent = ol.proj.transformExtent(mapConfig.restrictedExtent.split(",").map(Number), 'EPSG:4326', _map_projection);
+        }
 
 	//This control is used to display Lat and Lon when the user is moving the mouse over the map
 	var mousePositionControl = new ol.control.MousePosition({
@@ -136,8 +143,8 @@ function initOl3(){
 		maxZoom: mapConfig.zoomLevels,
 		zoom: mapConfig.zoom,
 		zoomFactor: mapConfig.zoomFactor,
-		maxResolution: mapConfig.maxResolution
-//		extent: resExtent  // Not working
+		maxResolution: mapConfig.maxResolution,
+		//extent: resExtent  // Not working
 	});
 
  	map = new ol.Map({
@@ -226,6 +233,8 @@ function initOl3(){
             else
                 map.once('change:ready', whenMapIsReady.bind(null, callback));
         }
+        
+        map.getView().fit(resExtent, map.getSize());
         
 }
 
