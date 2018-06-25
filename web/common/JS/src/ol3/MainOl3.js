38396901810bc2f016f0e2e7d64a62ee70981b78
mijailcc <mvagvcc@gmail.com>
2018-06-25 15:50:49 -0500

    modified:   web/common/JS/src/main/mapDisplay.js 	modified:   web/common/JS/src/ncwms/streamlines/NcWMSTwo.js 	modified:   web/common/JS/src/ol3/MainOl3.js

--- web/common/JS/src/ol3/MainOl3.js
+++ web/common/JS/src/ol3/MainOl3.js
@@ -53,9 +53,10 @@ function setMouseClickOnMap(){
  * This function animate position and zoom on the map
  * @returns undefined
  */
-function animatePositionMap(zoom, center, duration){
-    center = center === undefined ? ol3view.getCenter() : center;
-    duration = duration === undefined ? durationAnimation : duration;
+function animatePositionMap(zoom, center, duration, proj) {
+    proj = proj || PROJ_4326;
+    center = center ? ol.proj.transform(center, proj, _map_projection) : ol3view.getCenter();
+    duration = duration || durationAnimation;
     ol3view.animate({
                         center: center,
                         zoom: zoom,
@@ -241,7 +242,7 @@ function initOl3(){
      var center = layerDetails.center.split(",");
      if(center[0] !== "null" && center[1] !== "null") {
         map.once('change:ready', function() {
-            center = ol.proj.transform([parseInt(center[0]), parseInt(center[1])], PROJ_4326, _map_projection);
+            center = [parseInt(center[0]), parseInt(center[1])];
             var zoom = 'zoom' in layerDetails ? parseInt(layerDetails.zoom) : mapConfig.zoom;
             animatePositionMap(zoom, center, durationAnimation);
         });
@@ -277,13 +278,13 @@ owgis.ol3.positionMap = function(){
 	// --------------- Map visualization and hover texts
 	var newZoom = localStorage.zoom !== undefined && localStorage.zoom < 15? localStorage.zoom : ol3view.getZoom();// Zoom of map max 17 zoom
     var newCenter = ol3view.getCenter();
-	if( localStorage.map_center!== undefined){
+	if( localStorage.map_center !== undefined){
 		var strCenter = localStorage.map_center.split(",")
 		var lat = Number(strCenter[0]);
 		var lon = Number(strCenter[1]);
 		newCenter = [lat,lon];// Center of the map
-	}
-    animatePositionMap(newZoom, newCenter, 0);
+    }
+    animatePositionMap(newZoom, newCenter, 0, localStorage.projection);
 }
 
 /************************
