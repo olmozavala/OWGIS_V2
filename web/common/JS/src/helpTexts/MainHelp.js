8f9e4e6ad84e5b2e19391da98650e0a94f96b8cb
ixchel <ixchelzg@gmail.com>
2018-10-08 14:07:12 -0500

    traduccion de botones si es espa;ol en instrucciones

--- web/common/JS/src/helpTexts/MainHelp.js
+++ web/common/JS/src/helpTexts/MainHelp.js
@@ -6,41 +6,16 @@ goog.provide('owgis.help.main');
 owgis.help.main.displayHelp = function()
 {
     //$('#helpInstrContainer').toggle("fade");
-    /*var intro = introJs();
     
-    intro.setOptions({
-            steps: [
-              { 
-                intro: "Hello world!"
-              },
-              {
-                element: document.querySelector('#step1'),
-                intro: "This is a tooltip."
-              },
-              {
-                element: document.querySelectorAll('#step2')[0],
-                intro: "Ok, wasn't that fun?",
-                position: 'right'
-              },
-              {
-                element: '#step3',
-                intro: 'More features, more fun.',
-                position: 'left'
-              },
-              {
-                element: '#step4',
-                intro: "Another step.",
-                position: 'bottom'
-              },
-              {
-                element: '#step5',
-                intro: 'Get it, use it.'
-              }
-            ]
-    });
-    
-    intro.start();
-    */  
+    if(_curr_language == "ES"){
+        introJs().setOptions({
+                nextLabel: "siguiente",
+                prevLabel: "anterior",
+                skipLabel: "saltar",
+                doneLabel: "terminar"
+            });
+    }
+            
     introJs().start();
 
 }
