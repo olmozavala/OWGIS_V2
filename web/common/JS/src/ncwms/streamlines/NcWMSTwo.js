ff08f7485af9e131b1809a0a634c30609b136ea8
ixchel <ixchelzg@gmail.com>
2018-11-13 16:11:18 -0600

    reformatting

--- web/common/JS/src/ncwms/streamlines/NcWMSTwo.js
+++ web/common/JS/src/ncwms/streamlines/NcWMSTwo.js
@@ -11,20 +11,17 @@ owgis.ncwms.ncwmstwo.buildGridInfo = function buildGridInfo(data){
     var gridInfo = {};
     // Set min and max latitudes
     gridInfo.la1 = data.domain.axes.y.start; 
-	gridInfo.la2 = data.domain.axes.y.stop;
-	
-
-	// Set min and max longitudes
+    gridInfo.la2 = data.domain.axes.y.stop;
+    // Set min and max longitudes
     gridInfo.lo1 = data.domain.axes.x.start;
-	gridInfo.lo2 = data.domain.axes.x.stop;
-
-	// Set number of cells in the grid
-	gridInfo.ny = data.domain.axes.y.num;
-	gridInfo.nx = data.domain.axes.x.num;
+    gridInfo.lo2 = data.domain.axes.x.stop;
+    // Set number of cells in the grid
+    gridInfo.ny = data.domain.axes.y.num;
+    gridInfo.nx = data.domain.axes.x.num;
 	
-	// Set the delta x and delta y of the cells
-	gridInfo.dy = (gridInfo.la2 - gridInfo.la1)/gridInfo.ny;
-	gridInfo.dx = (gridInfo.lo2 - gridInfo.lo1)/gridInfo.nx;
+    // Set the delta x and delta y of the cells
+    gridInfo.dy = (gridInfo.la2 - gridInfo.la1)/gridInfo.ny;
+    gridInfo.dx = (gridInfo.lo2 - gridInfo.lo1)/gridInfo.nx;
 
     return gridInfo;
 }
@@ -37,15 +34,14 @@ owgis.ncwms.ncwmstwo.buildGridInfo = function buildGridInfo(data){
  * @returns {Array}
  */
 owgis.ncwms.ncwmstwo.buildGrid = function buildGrid(gridInfo, uData, vData){
-
-	var grid = new Array();
-	for (j = 0, p = 0; j < gridInfo.ny; j++) {
-		var row = [];
-		for (i = 0; i < gridInfo.nx; i++, p++) {
-			row[i] = [uData[p], vData[p]];
-		}
-		grid[gridInfo.ny-1-j] = row;
+    var grid = new Array();
+    for (j = 0, p = 0; j < gridInfo.ny; j++) {
+	var row = [];
+	for (i = 0; i < gridInfo.nx; i++, p++) {
+            row[i] = [uData[p], vData[p]];
+	}
+	grid[gridInfo.ny-1-j] = row;
     }
 	
-	return grid;
+    return grid;
 }
